96d299f5513774a7c33373a6a644119837b2e723
u-kai <76635578+u-kai@users.noreply.github.com>
2021-05-18 20:42:46 +0900

    create each init

--- src/hocks/useAutoCorrectPosition.ts
+++ src/hocks/useAutoCorrectPosition.ts
@@ -1,5 +1,5 @@
 import {useState} from "react"
 
 export const useAutoCorrectPosition = () => {
-
+    const [autoCorrectPosition,setAutoCorrectPosition] = useState({x:0,y:0})
 }
--- src/hocks/useFocusAutoCorrectIndex.ts
+++ src/hocks/useFocusAutoCorrectIndex.ts
@@ -1,5 +1,5 @@
 import {useState} from "react"
 
 export const useFocusAutoCorrectIndex = () => {
-    
+    const [focusAutoCorrectsIndex,setFocusAutoCorrectsIndex] = useState(0)
 }
