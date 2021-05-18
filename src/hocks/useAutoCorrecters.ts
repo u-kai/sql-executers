6b08323077e59fd5e77e1195fc3a45a765dd56f3
u-kai <76635578+u-kai@users.noreply.github.com>
2021-05-18 20:45:52 +0900

    create useAutoCorrecters

---
+++ src/hocks/useAutoCorrecters.ts
@@ -0,0 +1,9 @@
+import {useAutoCorrects} from "./useAutoCorrects"
+import {useFocusAutoCorrectIndex} from "./useFocusAutoCorrectIndex"
+import {useIsDisplayAutoCorrects} from "./useIsDisplayAutoCorrects"
+import {useAutoCorrectPosition} from "./useAutoCorrectPosition"
+
+
+export const useAutoCorrecters = () =>{
+    return {}
+}
