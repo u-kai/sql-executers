c3486fa779952a1579a8661ee08ba5760c975094
u-kai <76635578+u-kai@users.noreply.github.com>
2021-05-19 19:03:35 +0900

    create useAutoCorrectsPosition

--- src/hocks/useAutoCorrectPosition.ts
+++ src/hocks/useAutoCorrectPosition.ts
@@ -1,5 +1,22 @@
 import {useState} from "react"
 
+
 export const useAutoCorrectPosition = () => {
-    const [autoCorrectPosition,setAutoCorrectPosition] = useState({x:0,y:0})
+    const [autoCorrectsPosition,setAutoCorrectsPosition] = useState({x:0,y:0})
+    const getAutoCorrectsPosition = (focusRowIndex:number,prefixId="tailPosition") => {
+        const tail = document.getElementById(`${prefixId}${focusRowIndex}`)
+        const ajustTop = 20
+        let x = 0
+        let y = 0
+        if(tail){
+            x = window.pageXOffset + (tail.getBoundingClientRect().left)
+            y = window.pageYOffset + (tail.getBoundingClientRect().top) - ajustTop
+        }
+        return {x,y}
+    }
+    const getAndSetAutoCorrectsPosition = (focusRowIndex:number,prefixId="tailPosition") => {
+        const {x,y} = getAutoCorrectsPosition(focusRowIndex,prefixId)
+        setAutoCorrectsPosition({x:x,y:y})
+    }
+    return {autoCorrectsPosition, getAndSetAutoCorrectsPosition}
 }
