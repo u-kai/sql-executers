6f89b76dc765e5d943e113d4594bd870f6ec1c84
u-kai <76635578+u-kai@users.noreply.github.com>
2021-05-23 12:04:48 +0900

    create sentencesProvider

---
+++ src/providers/EditerProvider/Sentences.tsx
@@ -0,0 +1,12 @@
+import React,{createContext,useState} from "react"
+
+export const SentencesContext = createContext({})
+
+export const SnetencesProvider = () => {
+    const [sentences,setSentences] = useState<string[]>([])
+    return(
+        <SentencesContext.Provider
+            value={{sentences,setSentences}}
+            />
+    )
+}
